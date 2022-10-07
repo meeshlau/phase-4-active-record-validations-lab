c1e81a3cf330d3b075b12d0de5e0c3a9d38fb61b
Michelle Lau <meeshklau@gmail.com>
2022-10-06 18:14:24 -1000

    commit tree 854114a9e9cb4edf103efa2833c7b84eabe1d59a

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true, uniqueness: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :must_be_clickbaity
+
+    def must_be_clickbaity
+        unless title.present? && title.include?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+            errors.add(:title, 'Your title is not clickbait-y')
+        end
+    end
+
 end
